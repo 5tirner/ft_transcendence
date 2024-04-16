ee45ed65c775ad149450a409222153f54a38f92d
5tirner <thatfor1337@gmail.com>
2024-04-16 14:18:19 +0100

    @5tirner

---
+++ srcs/start/source/file.js
@@ -0,0 +1,27 @@
+//-------------------------------
+//console stuff
+console.log("Browser Console By Zasabri\n");
+//-------------------------------
+
+document.write("Today Date Is: " + Date());
+document.write("<br>");
+window.alert("Did You Want To See L'morphine's Math?");
+
+//-------------------------------
+//console stuff
+var a = 5, b = 5;
+a *= a, b *= b;
+var c = a + b;
+console.log("a = " + a + ", b = ", + b + '.');
+//-------------------------------
+
+document.write("If We Add " + a + " To " + b + " We Will Have: " + c);
+
+//-------------------------------
+//console stuff
+console.log(c);
+clore = 1 + 1 + "1";
+console.log("clore = " + clore);
+clore = "a" + " " + "b";
+console.log("clore = " + clore);
+//------------------------------
