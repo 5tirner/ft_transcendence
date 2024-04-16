95113a8e06b4ba037bb29f4d018b2f05f074103e
5tirner <thatfor1337@gmail.com>
2024-04-17 00:36:48 +0100

    zasabri

---
+++ srcs/infrastructer/source/instruction.js
@@ -0,0 +1,36 @@
+//-------------------------------
+//console stuff
+console.log("Browser Console By ZakariaSabri\n");
+//-------------------------------
+
+document.write("Today Date Is: " + Date());
+document.write("<br>");
+window.alert("Did You Want To See L'morphine's Math?");
+
+//-------------------------------
+//console stuff
+let a = 5, b = 5;
+a *= a, b *= b;
+let c = a + b;
+console.log("a = " + a + ", b = ", + b + '.');
+//-------------------------------
+// arr = ["Die", "Die", "Die"];
+// i = 0;
+// while (i < 3)
+// {
+//     document.write(arr[i]);
+//     document.write("<br>");
+//     i++;
+// }
+document.write("If We Add 5 To 5 We Will Have: " + c);
+
+//-------------------------------
+//console stuff
+console.log(c);
+clore = 1 + 1 + "1";
+let gogo;
+console.log("clore = " + clore);
+clore = "a" + " " + "b";
+console.log("clore = " + clore);
+console.log("GOGO: " + gogo);
+//------------------------------
