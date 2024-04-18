9ab6ee13442ef311c4348ff2134c8217fa6f1256
5tirner <thatfor1337@gmail.com>
2024-04-18 18:00:30 +0100

    zasabri
    Former-commit-id: cbd749d7c25d0231c80363216405bfa255b7d642

--- strings.js
+++ strings.js
@@ -1,7 +1,7 @@
 /*String*/
-str = "ABCDEFGHIJKL\"->\"MNOPQRSTUVWXYZ";
-console.log(str);
-console.log(str.length);
-str1 = new String("ABCDEFGHIJKL\"->\"MNOPQRSTUVWXYZ");
-console.log( str === str1);
-console.log( str1 == str);
+str1 = "ABCDEFGHIJKL\"->\"MNOPQRSTUVWXYZ";
+console.log("str1: " + str1); console.log("Len Of str1: " + str1.length);
+str2 = new String(str1);
+console.log("str2: " + str2); console.log("Len Of The Object str2: " + str2.length);
+console.log("Compare str1 & str2 As Objects === " + (str1 === str2));
+console.log("Compare The Two Strings str1 & str2 == " + (str1 == str2));
