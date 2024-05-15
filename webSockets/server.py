6282b8729b34d8d69cade1f15d4083a2cba623cd
5tirner <thatfor1337@gmail.com>
2024-05-15 20:13:42 +0100

    all

---
+++ webSockets/server.py
@@ -0,0 +1,11 @@
+import asyncio
+import websockets
+# create handler for each connection
+async def handler(websocket, path):
+    data = await websocket.recv()
+    reply = f"Data recieved as:{data}!"
+    await websocket.send(reply)
+start_server = websockets.serve(handler, "localhost", 8000)
+print("Type Of The Variable StartServer Is: ", type(start_server))
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
--- webSockets/sockets.py
+++
@@ -1,5 +0,0 @@
-import websockets
-import asyncio
-
-async def handler(websocket, path):
-    data = await websocket.recv()
