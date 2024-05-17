8355bd479cf7820c54150de88c3d8f267f6d01a4
5tirner <thatfor1337@gmail.com>
2024-05-17 22:19:34 +0100

    Start

--- tictactoe/dashboard/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('dashboard/', views.dashboard, name="dash"),
-    path('', views.welcome, name="start")
-]
--- tictactoe/dashboard/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-def dashboard(req):
-    template = loader.get_template('dash.html')
-    return (HttpResponse(template.render()))
-def welcome(req):
-    template = loader.get_template('welcome.html')
-    return (HttpResponse(template.render()))
