dd6c9a0fe692faf28e4cff616d349887badd0cb2
5tirner <thatfor1337@gmail.com>
2024-05-18 13:56:59 +0100

    Add List Frined With THe Infos

--- back_end/pingpong/models.py
+++ back_end/pingpong/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Players(models.Model):
+    username = models.CharField(max_length = 30)
+    level = models.IntegerField(default = 0)
+    gameplayed = models.IntegerField(default = 0)
+    gamewined = models.IntegerField(default = 0)
+    gamelosed = models.IntegerField(default = 0)
--- back_end/pingpong/urls.py
+++ back_end/pingpong/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('pingpong/', views.start, name="PingPong"),
     path('', views.welcome, name="Welcome Page"),
+    path('pingpong/show/<int:id>', views.details, name="User Details"),
 ]
