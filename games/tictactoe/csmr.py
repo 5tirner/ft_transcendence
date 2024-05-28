4536fc71592e732d403614149c3f96facf7f0537
5tirner <thatfor1337@gmail.com>
2024-05-28 17:30:07 +0100

    Add ws Protocol

--- games/tictactoe/csmr.py
+++ games/tictactoe/csmr.py
@@ -1,11 +1,13 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from .models import *
+from .models import tttgame
 
 class play(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope['ulr_route']['kwargs']['room_id']
-        self.room_group_name = 'room_%s' % self.room_name
-        print(self.room_name, '---', self.room_group_name)
-    async def disconnect():
+        await self.accept()
+        print("Connection Accepted Succefully")
+        # self.room_name = self.scope['ulr_route']['kwargs']['room_id']
+        # self.room_group_name = 'room_%s' % self.room_name
+        # print(self.room_name, '---', self.room_group_name)
+    async def disconnect(self, close_code):
         print("Disconnected")
