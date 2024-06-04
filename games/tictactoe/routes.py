c1f54fdbb395e32581f72de19c62b6b6f7ee4a89
Zakaria Sabri <zasabri@c4r2p6.1337.ma>
2024-06-04 19:06:08 +0100

    From Zero Again

--- games/tictactoe/routes.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .csmr import play
-
-wsurls = [
-    path('ws/game/<room_id>', play.as_asgi()),
-]
--- gaming/gaming/wsgi.py
+++ gaming/gaming/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for games project.
+WSGI config for gaming project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'games.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gaming.settings')
 
 application = get_wsgi_application()
