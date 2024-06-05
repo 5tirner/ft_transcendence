36257b373a521f8601a24bfa172819af63de30d7
brahim elkartouch <belkarto@student.1337.ma>
2024-06-05 18:12:57 +0000

    update 42 AUTH credential

--- backend/Authentication/restauthe/settings.py
+++ backend/Authentication/restauthe/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4)@8^!f1dbbppbdy&a(f7%ge&%7&n^=99_(&1)std@0irwo+e#'
+SECRET_KEY = "django-insecure-4)@8^!f1dbbppbdy&a(f7%ge&%7&n^=99_(&1)std@0irwo+e#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -33,63 +33,61 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework_simplejwt',
-    'restuserm.apps.RestusermConfig',
-    'rest_framework.authtoken',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "restuserm.apps.RestusermConfig",
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
     # 'restuserm.middleware.TokenMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'restauthe.urls'
+ROOT_URLCONF = "restauthe.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / 'templates'
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'restauthe.wsgi.application'
+WSGI_APPLICATION = "restauthe.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'admin',
-        'PASSWORD': '12345678',
-        'HOST': 'postgres_db',
-        'PORT': '5432',       # default PostgreSQL port
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "admin",
+        "PASSWORD": "12345678",
+        "HOST": "postgres_db",
+        "PORT": "5432",  # default PostgreSQL port
     }
 }
 
@@ -99,16 +97,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -116,9 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -128,12 +126,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # REST_FRAMEWORK = {
 #     'DEFAULT_AUTHENTICATION_CLASSES': [
@@ -142,59 +140,62 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # }
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
 
 
-#jwt-simple-token
+# jwt-simple-token
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
-    'REFRESH_TOKEN_LIFETIM': timedelta(days=1),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
-    'SLIDING_TOKEN_LIFETIME': timedelta(days=30),
-    'SLIDING_TOKEN_REFRESH_LIFETIME_LATE_USER': timedelta(days=1),
-    'SLIDING_TOKEN_LIFETIME_LATE_USER': timedelta(days=30),
-
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "REFRESH_TOKEN_LIFETIM": timedelta(days=1),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+    "SLIDING_TOKEN_LIFETIME": timedelta(days=30),
+    "SLIDING_TOKEN_REFRESH_LIFETIME_LATE_USER": timedelta(days=1),
+    "SLIDING_TOKEN_LIFETIME_LATE_USER": timedelta(days=30),
 }
 
 
 # Add this line to specify the directory containing static files
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # MEDIA_URL is the URL that handles the media served from MEDIA_ROOT
-MEDIA_URL = '/media/'
+MEDIA_URL = "/media/"
 
-PUBLIC_PLAYER_URL = 'http://localhost:8000/static/media/zakaria.jpg'
+PUBLIC_PLAYER_URL = "http://localhost:8000/static/media/zakaria.jpg"
 
 
 # 42/Google authentication variables
 
-FORTYTWO_CLIENT_ID = 'u-s4t2ud-276587daed10a1e33fcfbc113f9d4e97933508f4d8bb753cd1e7b49a31a00cb9'
-FORTYTWO_CLIENT_SECRET = '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'
+FORTYTWO_CLIENT_ID = (
+    "u-s4t2ud-276587daed10a1e33fcfbc113f9d4e97933508f4d8bb753cd1e7b49a31a00cb9"
+)
+FORTYTWO_CLIENT_SECRET = (
+    "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"
+)
 # FORTYTWO_REDIRECT_URI = 'https://api.intra.42.fr/oauth/authorize'
-FORTYTWO_REDIRECT_URI = 'http://127.0.0.1:8000/api/oauth/callback/'
+FORTYTWO_REDIRECT_URI = "http://127.0.0.1:8000/api/oauth/callback/"
 
 
-GOOGLE_CLIENT_ID = '1006058625597-bhtls9amhrpcuu5ltdungv65kehm56ch.apps.googleusercontent.com'
-GOOGLE_CLIENT_SECRET = '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'
-GOOGLE_REDIRECT_URI = 'http://127.0.0.1:8000/api/google/callback/'
+GOOGLE_CLIENT_ID = (
+    "1006058625597-bhtls9amhrpcuu5ltdungv65kehm56ch.apps.googleusercontent.com"
+)
+GOOGLE_CLIENT_SECRET = "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"
+GOOGLE_REDIRECT_URI = "http://127.0.0.1:8000/api/google/callback/"
 
-TRANSCE_HOST = '127.0.0.1:8000/api'
+TRANSCE_HOST = "127.0.0.1:8000/api"
 
 
-#For temporary Users Creation
+# For temporary Users Creation
 
 AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
+    "django.contrib.auth.backends.ModelBackend",
     # Add any additional authentication backends if needed
 ]
