d1db3eead47861f675aee428e0b30ef12fcf1cff
brahim elkartouch <belkarto@student.1337.ma>
2024-07-03 14:23:07 +0100

    add event listener in friendsList

--- frontend/srcs/scripts/components/chat/chat.js
+++ frontend/srcs/scripts/components/chat/chat.js
@@ -8,20 +8,3 @@ export async function render_chat() {
 	await getConversations();
 	init_socket();
 }
-
-var drop_down_menu = document.querySelector(".dropdown-content ");
-
-window.addEventListener("click", async (event) => {
-	if (!event.target.matches(".add-message-icon img")) {
-		if (drop_down_menu.classList.contains("show")) {
-			drop_down_menu.classList.remove("show");
-		}
-	} else {
-		if (drop_down_menu.classList.contains("show")) {
-			drop_down_menu.classList.remove("show");
-		} else {
-			await fillFriensList();
-			drop_down_menu.classList.add("show");
-		}
-	}
-});
--- frontend/srcs/scripts/components/chat/friendsList.js
+++ frontend/srcs/scripts/components/chat/friendsList.js
@@ -8,8 +8,28 @@ export async function fillFriensList(params) {
 		const { friendships } = response;
 		friendships.forEach((elem) => {
 			const userElem = document.createElement("div");
+			userElem.addEventListener("click", (event) => {
+				console.log(event);
+			});
 			userElem.textContent = elem.username;
 			dropDown.appendChild(userElem);
 		});
 	}
 }
+
+var drop_down_menu = document.querySelector(".dropdown-content ");
+
+window.addEventListener("click", async (event) => {
+	if (!event.target.matches(".add-message-icon img")) {
+		if (drop_down_menu.classList.contains("show")) {
+			drop_down_menu.classList.remove("show");
+		}
+	} else {
+		if (drop_down_menu.classList.contains("show")) {
+			drop_down_menu.classList.remove("show");
+		} else {
+			await fillFriensList();
+			drop_down_menu.classList.add("show");
+		}
+	}
+});
