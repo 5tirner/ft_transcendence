5225f0ec48aedc75a9c15a2e560b302a19210da1
brahim elkartouch <belkarto@student.1337.ma>
2024-07-08 15:39:13 +0100

    fix duplicated text when reciving msgs

--- makeusers.py
+++ makeusers.py
@@ -3,6 +3,9 @@ import requests
 authAPI = "http://127.0.0.1:8000/api/signups/"
 loginAPI = "http://127.0.0.1:8000/api/logins/"
 creatChatroomAPI = "http://127.0.0.1:8000/api/chat/create/"
+friendshipAPI = "http://127.0.0.1:8000/api/friendship/"
+
+USER_NUMBER = 5
 
 
 def create_users():
@@ -12,16 +15,11 @@ def create_users():
 
     # print(2 * "\t", "================= register test users ==================")
     data = {"email": "", "username": "", "password": "1234"}
-    # create login user
-    data["email"] = f"user@user.com"
-    data["username"] = f"user"
-    res = requests.post(authAPI, data=data)
-    # print(f"user respons ==>", res.json(), res.status_code)
-    for i in range(5):
+    for i in range(USER_NUMBER):
         data["email"] = f"user{i}@user.com"
         data["username"] = f"user{i}"
         res = requests.post(authAPI, data=data)
-        # print(f"user{i} respons ==>", res.json(), res.status_code)
+    #     print(f"user{i} respons ==>", res.json(), res.status_code)
     # print(2 * "\t", "=======================================================", 2 * "\n")
 
 
@@ -30,40 +28,43 @@ def get_auth_cookie(user):
     res = requests.post(loginAPI, data=login_data)
     jwt_token = res.cookies["jwt_token"]
     cooki = {"jwt_token": jwt_token}
-    print(f"{user} ====>\n{jwt_token}")
+    print("====> \n", jwt_token)
     return cooki
 
 
 def create_chatrooms():
     # print(2 * "\t", "================= create chatrooms ====================")
+
     user = {"username": ""}
-    cookie = get_auth_cookie("user@user.com")
-    user["username"] = f"belkarto"
-    res = requests.post(creatChatroomAPI, data=user, cookies=cookie)
-    for i in range(5):
-        user["username"] = f"user{i}"
+
+    for id in range(USER_NUMBER):
+        user_mail = f"user{id}@user.com"
+        cookie = get_auth_cookie(user_mail)
+        for i in range(USER_NUMBER):
+            user["username"] = f"user{i}"
+            res = requests.post(creatChatroomAPI, data=user, cookies=cookie)
+            # print(f"user{i} respons ==>", res.json(), res.status_code)
+
+        user["username"] = "belkarto"
         res = requests.post(creatChatroomAPI, data=user, cookies=cookie)
-        # print(f"user{i} respons ==>", res.json(), res.status_code)
 
     # print(2 * "\t", "=======================================================")
-    cookie = get_auth_cookie("user0@user.com")
-    user["username"] = f"belkarto"
-    res = requests.post(creatChatroomAPI, data=user, cookies=cookie)
-    for i in range(5):
-        user["username"] = f"user{i}"
-        res = requests.post(creatChatroomAPI, data=user, cookies=cookie)
-    #     print(f"user{i} respons ==>", res.json(), res.status_code)
-    # print(2 * "\t", "=======================================================")
 
-    cookie = get_auth_cookie("user1@user.com")
-    user["username"] = f"belkarto"
-    res = requests.post(creatChatroomAPI, data=user, cookies=cookie)
-    for i in range(5):
-        user["username"] = f"user{i}"
-        res = requests.post(creatChatroomAPI, data=user, cookies=cookie)
-    #     print(f"user{i} respons ==>", res.json(), res.status_code)
+
+def create_friendship():
+    # print(2 * "\t", "================= create friendship ===================")
+    friend = {"id_target": 1}
+    for id in range(USER_NUMBER):
+        user_mail = f"user{id}@user.com"
+        cookie = get_auth_cookie(user_mail)
+        for i in range(USER_NUMBER):
+            friend["id_target"] = i + 1
+            res = requests.post(friendshipAPI, data=friend, cookies=cookie)
+            # print(f"user{i} respons ==>", res.json(), res.status_code)
+
     # print(2 * "\t", "=======================================================")
 
 
 create_users()
 create_chatrooms()
+create_friendship()
