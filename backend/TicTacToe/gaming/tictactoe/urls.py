10d5f28c3642d4db3ae52d29e0854ff8ee55225c
5tirner <thatfor1337@gmail.com>
2024-07-13 18:07:47 +0100

    API FOR SEE OTHER BASIC INFORMATION

--- backend/TicTacToe/gaming/tictactoe/urls.py
+++ backend/TicTacToe/gaming/tictactoe/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import game, statistics
+from .views import game, statistics, userStatistic
 
 urlpatterns = [
     path('MyStatistics/', statistics, name="homePage"),
+    path('UserStat/<login>/', userStatistic),
     path('TicTacToe/', game, name="tictactoe")
 ]
