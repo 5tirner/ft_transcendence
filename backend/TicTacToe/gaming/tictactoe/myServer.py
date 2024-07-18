56955f0c0469d69ba844c9e295735a67d037ef64
5tirner <thatfor1337@gmail.com>
2024-07-18 15:26:54 +0100

    Connection Problemes And MatchMaking End

--- backend/TicTacToe/gaming/tictactoe/myServer.py
+++ backend/TicTacToe/gaming/tictactoe/myServer.py
@@ -26,33 +26,70 @@ class myServerOnLobby(AsyncWebsocketConsumer):
 
 class myServerOnGame(AsyncWebsocketConsumer):
     playerWantsToPlay = list()
+    playersOnMatchAndItsRoomId = dict()
+    playersOnMatchAndItsOppenent = dict()
     async def connect(self):
         print(f'----------User On GAME Is: {self.scope["user"]}-------')
         if len(self.playerWantsToPlay) == 0:
             player1, player2 = self.scope['user'], ""
-            roomid = gameInfo.objects.get(login=self.scope["user"]).codeToPlay
-            self.playerWantsToPlay.append(self.scope['user'])
-            print(f"{self.scope['user']} Alone In The Q, He Waiting")
-            await self.channel_layer.group_add(roomid, self.channel_name)
+            if self.playersOnMatchAndItsRoomId.get(player1) is not None:
+                print(f"Can't Add Player {player1} To Q His Alraedy In Match")
+                await self.close()
+            else:
+                roomid = gameInfo.objects.get(login=self.scope["user"]).codeToPlay
+                self.playerWantsToPlay.append(self.scope['user'])
+                print(f"{self.scope['user']} Alone In The Q, He Waiting")
+                await self.channel_layer.group_add(roomid, self.channel_name)
+                await self.accept()
+                toFronEnd = json.dumps({'player1': player1, 'player2': player2, 'roomid': roomid})
+                print(f"Player1: {player1}, Player2: {player2}, RoomId: {roomid}")
+                await self.channel_layer.group_send(roomid, {'type': 'ToFrontOnConnect', 'Data': toFronEnd})
         else:
             player1, player2 = self.playerWantsToPlay[0], self.scope['user']
-            print(f"{self.scope['user']} Will Joinned To The Player {self.playerWantsToPlay[0]}")
-            roomid = gameInfo.objects.get(login=self.playerWantsToPlay[0]).codeToPlay
-            await self.channel_layer.group_add(roomid, self.channel_name)
-            self.playerWantsToPlay.remove(self.playerWantsToPlay[0])
+            if self.playersOnMatchAndItsRoomId.get(player2) is not None:
+                print(f"Can't Add Player {player2} To Game With {player1} His Alraedy In Match")
+                await self.close()
+            else:
+                print(f"{self.scope['user']} Will Joinned To The Player {self.playerWantsToPlay[0]}")
+                roomid = gameInfo.objects.get(login=self.playerWantsToPlay[0]).codeToPlay
+                self.playersOnMatchAndItsOppenent[player1] = player2
+                self.playersOnMatchAndItsOppenent[player2] = player1
+                self.playersOnMatchAndItsRoomId[player1] = roomid
+                self.playersOnMatchAndItsRoomId[player2] = roomid
+                await self.channel_layer.group_add(roomid, self.channel_name)
+                self.playerWantsToPlay.remove(self.playerWantsToPlay[0])
+                await self.accept()
+                toFrontEnd = json.dumps({'player1': player1, 'player2': player2, 'roomid': roomid})
+                print(f"Player1: {player1}, Player2: {player2}, RoomId: {roomid}")
+                await self.channel_layer.group_send(roomid, {'type': 'ToFrontOnConnect', 'Data': toFrontEnd})
             print(f"Still In Q: {len(self.playerWantsToPlay)}")
-        await self.accept()
-        toFronEnd = json.dumps({'player1': player1, 'player2': player2, 'roomid': roomid})
-        print(f"Player1: {player1}, Player2: {player2}, RoomId: {roomid}")
-        await self.channel_layer.group_send(roomid, {'type': 'ToFront', 'Data': toFronEnd})
     async def receive(self, text_data, bytes_data=0):
         print(f'----------Data Come From User: {self.scope["user"]}-------')
-        print(f"Data: {text_data}")
+        print(f"Data From Type: {type(text_data)} ==> {text_data}")
         print("DONE!")
+        thisUser, hisOppenent = self.scope["user"], self.playersOnMatchAndItsOppenent.get(self.scope["user"])
+        roomidForThisUser = self.playersOnMatchAndItsRoomId.get(thisUser)
+        print(f"Id To Send Data: {roomidForThisUser}")
+        toFrontEnd = json.dumps({'etat': "PLAYING", 'user': thisUser, 'oppenent': hisOppenent})
+        if roomidForThisUser is not None:
+            await self.channel_layer.group_send(roomidForThisUser, {'type': 'ToFrontOnPlaying', 'Data': toFrontEnd})
     async def disconnect(self, code):
         print(f"Connection Of User: {self.scope['user']} Lost")
-        self.channel_layer.group_discard(roomid, self.channel_name)
+        try:
+            roomidForThisUser = self.playersOnMatchAndItsRoomId.get(self.scope["user"])
+            player1, player2 = self.scope['user'], self.playersOnMatchAndItsOppenent.get(self.scope['user'])
+            self.playersOnMatchAndItsOppenent.pop(player1)
+            self.playersOnMatchAndItsOppenent.pop(player2)
+            self.playersOnMatchAndItsRoomId.pop(player1)
+            self.playersOnMatchAndItsRoomId.pop(player2)
+            await self.channel_layer.group_discard(roomidForThisUser, self.channel_name)
+        except:
+            pass
     
-    async def ToFront(self, data):
+    async def ToFrontOnConnect(self, data):
+        print("Sending Data To Clinet...")
+        await self.send(data['Data'])
+    
+    async def ToFrontOnPlaying(self, data):
         print("Sending Data To Clinet...")
         await self.send(data['Data'])
