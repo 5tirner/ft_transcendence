d2aa2e2c21f788958897cb67516581b7fe165461
5tirner <thatfor1337@gmail.com>
2024-07-18 17:30:02 +0100

    The Second Game Is NEd

--- backend/TicTacToe/gaming/tictactoe/models.py
+++ backend/TicTacToe/gaming/tictactoe/models.py
@@ -11,11 +11,11 @@ class gameInfo(models.Model):
 class onLobby(models.Model):
     login = models.CharField(max_length=20)
 
-class onGame(models.Model):
-    player1 = models.CharField(max_length=20)
-    player2 = models.CharField(max_length=20)
-    turn = models.IntegerField(default=1)
-    roomid = models.CharField(max_length=20)
+# class onGame(models.Model):
+#     player1 = models.CharField(max_length=20)
+#     player2 = models.CharField(max_length=20)
+#     turn = models.IntegerField(default=1)
+#     roomid = models.CharField(max_length=20)
 
 class history(models.Model):
     you = models.CharField(max_length=20)
--- backend/TicTacToe/gaming/tictactoe/myServer.py
+++ backend/TicTacToe/gaming/tictactoe/myServer.py
@@ -1,8 +1,8 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 from .models import onLobby, gameInfo
-from threading import Thread
 import os
+from .checkClick import isLegalClick
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 class myServerOnLobby(AsyncWebsocketConsumer):
@@ -28,6 +28,8 @@ class myServerOnGame(AsyncWebsocketConsumer):
     playerWantsToPlay = list()
     playersOnMatchAndItsRoomId = dict()
     playersOnMatchAndItsOppenent = dict()
+    playersOnMatchAndItsRole = dict()
+
     async def connect(self):
         print(f'----------User On GAME Is: {self.scope["user"]}-------')
         if len(self.playerWantsToPlay) == 0:
@@ -56,6 +58,8 @@ class myServerOnGame(AsyncWebsocketConsumer):
                 self.playersOnMatchAndItsOppenent[player2] = player1
                 self.playersOnMatchAndItsRoomId[player1] = roomid
                 self.playersOnMatchAndItsRoomId[player2] = roomid
+                self.playersOnMatchAndItsRole[player1] = 'X'
+                self.playersOnMatchAndItsRole[player2] = 'O'
                 await self.channel_layer.group_add(roomid, self.channel_name)
                 self.playerWantsToPlay.remove(self.playerWantsToPlay[0])
                 await self.accept()
@@ -63,16 +67,37 @@ class myServerOnGame(AsyncWebsocketConsumer):
                 print(f"Player1: {player1}, Player2: {player2}, RoomId: {roomid}")
                 await self.channel_layer.group_send(roomid, {'type': 'ToFrontOnConnect', 'Data': toFrontEnd})
             print(f"Still In Q: {len(self.playerWantsToPlay)}")
+
     async def receive(self, text_data, bytes_data=0):
         print(f'----------Data Come From User: {self.scope["user"]}-------')
         print(f"Data From Type: {type(text_data)} ==> {text_data}")
         print("DONE!")
-        thisUser, hisOppenent = self.scope["user"], self.playersOnMatchAndItsOppenent.get(self.scope["user"])
-        roomidForThisUser = self.playersOnMatchAndItsRoomId.get(thisUser)
-        print(f"Id To Send Data: {roomidForThisUser}")
-        toFrontEnd = json.dumps({'etat': "PLAYING", 'user': thisUser, 'oppenent': hisOppenent})
-        if roomidForThisUser is not None:
-            await self.channel_layer.group_send(roomidForThisUser, {'type': 'ToFrontOnPlaying', 'Data': toFrontEnd})
+        dataFromClient = json.loads(text_data)
+        try:
+            thisUser, hisOppenent = self.scope["user"], self.playersOnMatchAndItsOppenent.get(self.scope["user"])
+            if self.playersOnMatchAndItsRoomId.get(thisUser) == gameInfo.objects.get(login=thisUser).codeToPlay:
+                symbol = 'X'
+            else:
+                symbol = 'O'
+            board = dataFromClient.get('board')
+            isLegalClick(board, symbol, thisUser)
+            pos = dataFromClient.get('position')
+            board = board[:pos] + symbol + board[pos + 1:]
+            roomidForThisUser = self.playersOnMatchAndItsRoomId.get(thisUser)
+            print(f"Id To Send Data: {roomidForThisUser}")
+            toFrontEnd = json.dumps({
+                'etat': "PLAYING",
+                'user': thisUser,
+                'oppenent': hisOppenent,
+                'position': dataFromClient.get('position'),
+                'x_o': symbol,
+                'board': board
+                })
+            if roomidForThisUser is not None:
+                await self.channel_layer.group_send(roomidForThisUser, {'type': 'ToFrontOnPlaying', 'Data': toFrontEnd})
+        except:
+            pass
+
     async def disconnect(self, code):
         print(f"Connection Of User: {self.scope['user']} Lost")
         try:
@@ -82,6 +107,8 @@ class myServerOnGame(AsyncWebsocketConsumer):
             self.playersOnMatchAndItsOppenent.pop(player2)
             self.playersOnMatchAndItsRoomId.pop(player1)
             self.playersOnMatchAndItsRoomId.pop(player2)
+            self.playersOnMatchAndItsRole.pop(player1)
+            self.playersOnMatchAndItsRole.pop(player2)
             await self.channel_layer.group_discard(roomidForThisUser, self.channel_name)
         except:
             pass
