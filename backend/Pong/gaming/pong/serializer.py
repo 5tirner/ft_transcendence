9efea7cea2a4a9e005fbe0520c740b59bbf8cf72
5tirner <thatfor1337@gmail.com>
2024-08-16 09:02:40 +0100

    Pull Req

--- backend/Pong/gaming/pong/serializer.py
+++ backend/Pong/gaming/pong/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import pongGameInfo
+from .models import pongGameInfo, playerAndHisPic, pongHistory
 
 class pongGameInfoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,4 +7,13 @@ class pongGameInfoSerializer(serializers.ModelSerializer):
         fields = ['login', 'wins', 'loses', 'draws', 'gamesPlayed', 'codeToPlay']
 
 
+class pongAvatarsSerilizer(serializers.ModelSerializer):
+    class Meta:
+        model = playerAndHisPic
+        fields = ['login', 'pic']
+
+class pongHistoryInfos(serializers.ModelSerializer):
+    class Meta:
+        model = pongHistory
+        fields = ['you', 'oppenent', 'winner']
 
