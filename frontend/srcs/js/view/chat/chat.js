5cefd544019e53bd2117147ebeb7d50fa42bcb56
brahim elkartouch <belkarto@student.1337.ma>
2024-08-16 04:02:39 +0100

    start adding chat again to new UI

--- frontend/srcs/js/view/chat/chat.js
+++ frontend/srcs/js/view/chat/chat.js
@@ -1,9 +1,18 @@
-import API from "../../service/API.js";
-import { getConversations } from "./chatList.js";
-import { fillFriensList } from "./friendsList.js";
-import { init_socket } from "./socketConnection.js";
+// import API from "../../service/API.js";
+// import { getConversations } from "./chatList.js";
+// import { fillFriensList } from "./friendsList.js";
+// import { init_socket } from "./socketConnection.js";
 
-export async function render_chat() {
-	await getConversations();
-	init_socket();
+// export async function render_chat() {
+// 	await getConversations();
+// 	init_socket();
+// }
+
+export class ChatComponent extends HTMLDivElement {
+	constructor() {
+		super();
+	}
+	connectedCallback() {
+		this.textContent = "this is a fucking custom div";
+	}
 }
