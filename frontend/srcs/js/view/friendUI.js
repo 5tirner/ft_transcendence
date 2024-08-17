8c0aee71d07a219fdb9978e273184728dfa1faf5
brahim elkartouch <belkarto@student.1337.ma>
2024-08-17 19:28:59 +0100

    add friend management

--- frontend/srcs/js/view/friendUI.js
+++ frontend/srcs/js/view/friendUI.js
@@ -4,28 +4,52 @@ export class FriendView extends HTMLElement {
 	}
 	connectedCallback() {
 		this.innerHTML = `
+        <p>Friends Management</p>
 		<div class="friend-container">
-			<p>Friends Management</p>
 
 			<div class="friend-card" id="friends-list">
 				<p>Friends List</p>
-				<ul id="friends"></ul>
+				<ul id="friends">
+                    <li>user</li>
+                    <li>user</li>
+                    <li>user</li>
+                    <li>user</li>
+                </ul>
 			</div>
-			<!---->
-			<!-- <div class="friend-card" id="all-users"> -->
-			<!-- 	<h2>All Users</h2> -->
-			<!-- 	<ul id="users"></ul> -->
-			<!-- </div> -->
-			<!---->
-			<!-- <div class="friend-card" id="received-requests"> -->
-			<!-- 	<h2>Received Friend Requests</h2> -->
-			<!-- 	<ul id="requests"></ul> -->
-			<!-- </div> -->
-			<!---->
-			<!-- <div class="friend-card" id="blocked-users"> -->
-			<!-- 	<h2>Blocked Users</h2> -->
-			<!-- 	<ul id="blocked"></ul> -->
-			<!-- </div> -->
+
+			<div class="friend-card" id="all-users">
+				<p>All Users</p>
+				<ul id="users">
+                    <li>user</li>
+                    <li>user</li>
+                    <li>user</li>
+                    <li>user</li>
+                </ul>
+			</div>
+
+			<div class="friend-card" id="received-requests">
+				<p>Received Friend Requests</p>
+				<ul id="requests">
+                    <li>user</li>
+                    <li>user</li>
+                    <li>user</li>
+                    <li>user</li>
+                </ul>
+			</div>
+
+			<div class="friend-card" id="blocked-users">
+				<p>Blocked Users</p>
+				<ul id="blocked">
+                    <li>user</li>
+                    <li>user</li>
+                    <li>user</li>
+                    <li>5 user</li>
+                    <li>4 user</li>
+                    <li>3 user</li>
+                    <li>2 user</li>
+                    <li>1 user</li>
+                </ul>
+			</div>
 		</div>
 `;
 	}
