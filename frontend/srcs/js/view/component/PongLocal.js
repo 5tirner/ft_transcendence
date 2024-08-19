1b8808d287f7abd9076c45f41220789fda369adf
5tirner <thatfor1337@gmail.com>
2024-08-19 12:13:31 +0100

    Pull Req

--- frontend/srcs/js/view/component/PongLocal.js
+++ frontend/srcs/js/view/component/PongLocal.js
@@ -98,7 +98,7 @@ export default class PongLocal extends HTMLElement
 		socket.ws.onclose = function () {
 		  this.isGameStarted == false;
 			console.log("BYE FROM SERVER");
-			clearInterval(this.SaveInterval);
+			// clearInterval(this.SaveInterval);
 		};
 		
 		socket.ws.onopen = function () {
@@ -113,37 +113,39 @@ export default class PongLocal extends HTMLElement
 	
 	drawElements()
 	{
-    console.log("Start Drawing Elements");
-    if (!this.isGameStarted) return;
-		this.ballMove();
-		this.canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    // console.log("Start Drawing Elements");
+    	if (this.isGameStarted == true)
+		{
+			this.ballMove();
+			this.canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-		this.canvasContext.beginPath();
-		this.canvasContext.arc(this.xBallPos, this.yBallPos, 10, 0, 6.2);
-		this.canvasContext.lineWidth = 0.5;
-		this.canvasContext.fillStyle = "#F0F8FF";
-		this.canvasContext.fill();
-		this.canvasContext.closePath();
-		this.canvasContext.strokeStyle = "rgb(140, 29, 260)";
-		this.canvasContext.stroke();
+			this.canvasContext.beginPath();
+			this.canvasContext.arc(this.xBallPos, this.yBallPos, 10, 0, 6.2);
+			this.canvasContext.lineWidth = 0.5;
+			this.canvasContext.fillStyle = "#F0F8FF";
+			this.canvasContext.fill();
+			this.canvasContext.closePath();
+			this.canvasContext.strokeStyle = "rgb(140, 29, 260)";
+			this.canvasContext.stroke();
 
-		this.canvasContext.beginPath();
-		this.canvasContext.lineWidth = 8;
-		this.canvasContext.moveTo(20, this.paddl1Y);
-		this.canvasContext.lineTo(20, this.paddl1Y + 50);
-		this.canvasContext.closePath();
-		this.canvasContext.strokeStyle = "#F0F8FF";
-		this.canvasContext.stroke();
+			this.canvasContext.beginPath();
+			this.canvasContext.lineWidth = 8;
+			this.canvasContext.moveTo(20, this.paddl1Y);
+			this.canvasContext.lineTo(20, this.paddl1Y + 50);
+			this.canvasContext.closePath();
+			this.canvasContext.strokeStyle = "#F0F8FF";
+			this.canvasContext.stroke();
 
-		this.canvasContext.beginPath();
-		this.canvasContext.lineWidth = 8;
-		this.canvasContext.moveTo(580, this.paddl2Y);
-		this.canvasContext.lineTo(580, this.paddl2Y + 50);
-		this.canvasContext.closePath();
-		this.canvasContext.strokeStyle = "#F0F8FF";
-		this.canvasContext.stroke();
+			this.canvasContext.beginPath();
+			this.canvasContext.lineWidth = 8;
+			this.canvasContext.moveTo(580, this.paddl2Y);
+			this.canvasContext.lineTo(580, this.paddl2Y + 50);
+			this.canvasContext.closePath();
+			this.canvasContext.strokeStyle = "#F0F8FF";
+			this.canvasContext.stroke();
+		}
 	}
-	
+
 	start()
 	{
 		this.isGameStarted = true;
@@ -171,17 +173,18 @@ export default class PongLocal extends HTMLElement
 	
 	ballMove()
 	{
-    console.log("Start Moving The Ball");
+    // console.log("Start Moving The Ball");
 		if (this.xBallPos <= 0 || this.xBallPos >= 600)
 		{
-		  console.log("Sus 1");
+		  	console.log("Sus 1");
+			this.removeEventListener('keyup', (e) => this.applyMove(e))
 			this.isGameStarted = false;
 			socket.ws.send(JSON.stringify({ gameStatus: "End" }));
 			clearInterval(this.SaveInterval);
 		}
 		else if (this.isGameStarted == true)
 		{
-		  console.log("Sus 2");
+		//   console.log("Sus 2");
 			const ToServer =
 			{
 				WhatIGiveYou: "BALL MOVE",
@@ -200,9 +203,9 @@ export default class PongLocal extends HTMLElement
 	
 	applyMove(e)
 	{
-    console.log("Apply Moves");
 		if (this.isGameStarted == true)
 		{
+			// console.log("Apply Moves");
 			if ( e.key == "ArrowUp" || e.key == "ArrowDown" || e.key == "w" || e.key == "s" )
 			{
 				const ToServer =
