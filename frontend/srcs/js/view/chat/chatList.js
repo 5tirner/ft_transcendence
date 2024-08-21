6399265787911142b6857a9c61059d47b3c3fb24
brahim elkartouch <belkarto@student.1337.ma>
2024-08-21 20:16:22 +0000

    change in chat

--- frontend/srcs/js/view/chat/chatList.js
+++ frontend/srcs/js/view/chat/chatList.js
@@ -48,21 +48,6 @@ export function formatListDate(date) {
 	}
 }
 
-export async function getConversations() {
-	const ulElement = document.querySelector('.list-group')
-  console.log("ulelemn",ulElement);
-	let response = await API.getConversatons();
-	if (response.ok) {
-		response = await response.json();
-		response.forEach((chatConv) => {
-		 console.log(response)
-			let conv = new ConvElement();
-			conv.data = chatConv;
-			ulElement.appendChild(conv);
-		});
-	}
-}
-
 export function findUserInList(username) {
 	const listItems = document.querySelectorAll(".list-group-item");
 	if (!listItems) return null;
