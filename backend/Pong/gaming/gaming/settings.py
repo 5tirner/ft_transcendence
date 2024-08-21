4e13dc86ed0f24a888bf0d16d7c64d554d398646
5tirner <thatfor1337@gmail.com>
2024-08-21 17:14:48 +0100

    @5tirner Update

--- backend/Pong/gaming/gaming/settings.py
+++ backend/Pong/gaming/gaming/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
 
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
 #         "NAME": DB_NAME,
 #         "USER": DB_USER,
 #         "PASSWORD": DB_PASSWORD,
