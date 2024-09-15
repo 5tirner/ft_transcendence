8ee0d259bd884e3bd2bfb73c38466ddd7564e3ed
5tirner <thatfor1337@gmail.com>
2024-09-15 10:17:33 +0100

    Fix Unique Login Problem And Use PostGres As Games's DataBase

--- backend/TicTacToe/gaming/gaming/settings.py
+++ backend/TicTacToe/gaming/gaming/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,21 +79,29 @@ ASGI_APPLICATION = 'gaming.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_USER = os.getenv("POSTGRES_USER")
+DB_NAME = os.getenv("POSTGRES_DB")
+DB_HOST = os.getenv("POSTGRES_HOST")
+DB_PORT = os.getenv("POSTGRES_PORT")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": DB_NAME,
+        "USER": DB_USER,
+        "PASSWORD": DB_PASSWORD,
+        "HOST": DB_HOST,
+        "PORT": DB_PORT,  # default PostgreSQL port
     }
 }
-
-# REST_FRAMEWORK = {
-#     # Use Django's standard `django.contrib.auth` permissions,
-#     # or allow read-only access for unauthenticated users.
-#     'DEFAULT_PERMISSION_CLASSES': [
-#         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-#     ]
-
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
--- backend/TicTacToe/gaming/tictactoe/models.py
+++ backend/TicTacToe/gaming/tictactoe/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
 class gameInfo(models.Model):
-    login = models.CharField(max_length=20)
+    login = models.CharField(max_length=30)
     wins = models.IntegerField(default=0)
     loses = models.IntegerField(default=0)
     draws = models.IntegerField(default=0)
     gamesPlayed = models.IntegerField(default=0)
-    codeToPlay = models.CharField(max_length=20)
+    codeToPlay = models.CharField(max_length=30)
 
 # class onLobby(models.Model):
 #     login = models.CharField(max_length=20)
 
 class playerAndHisPic(models.Model):
-    login=models.CharField(max_length=20)
-    pic=models.CharField(max_length=100)
+    login=models.CharField(max_length=30)
+    pic=models.CharField(max_length=200)
 
 class history(models.Model):
-    you = models.CharField(max_length=20)
-    oppenent = models.CharField(max_length=20)
-    winner = models.CharField(max_length=20)
+    you = models.CharField(max_length=30)
+    oppenent = models.CharField(max_length=30)
+    winner = models.CharField(max_length=30)
