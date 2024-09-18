3f33da381264bc9d6bd00de0cb02967749703a62
brahim elkartouch <belkarto@student.1337.ma>
2024-09-18 06:16:05 +0000

    remove conflict and migration problem

--- backend/chat/core/auth_middleware.py
+++ backend/chat/core/auth_middleware.py
@@ -1,7 +1,21 @@
 import requests
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
-from restuserm.models import Player  # Import your Player model
+
+# from restuserm.models import Player  # Import your Player model
+
+
+class CustomUser:
+    def __init__(self, user_data):
+        self.id = user_data.get("id")
+        self.username = user_data.get("username")
+        self.email = user_data.get("email")
+        self.first_name = user_data.get("first_name")
+        self.last_name = user_data.get("last_name")
+        # Add more fields if necessary
+
+    def is_authenticated(self):
+        return True
 
 
 class JwtAuth(BaseAuthentication):
@@ -27,8 +41,5 @@ class JwtAuth(BaseAuthentication):
 
         if not user_id:
             return None
-        try:
-            player = Player.objects.get(id=user_id)
-        except Player.DoesNotExist:
-            raise AuthenticationFailed("Player not found")
-        return (player, None)
+        current_user = CustomUser(user_data)
+        return (current_user, None)
